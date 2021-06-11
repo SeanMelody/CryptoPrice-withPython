742eeaaac077eb7ee7dd1137ba0a072ac57060cf
Sean Melody <seandmelody@gmail.com>
2021-06-11 12:31:46 -0700

    Beautfiul Soup working

---
+++ main.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+# Printing Test Start
+# import string
+# import random
+
+# print("crypto")
+
+# print(random.sample(string.ascii_letters, 10))
+# Printing Test End
+
+# BS4 TEST START
+# with open("index.html") as fp:
+#     soup = BeautifulSoup(fp, 'html.parser')
+
+# soup = BeautifulSoup("<html>a web page</html>", 'html.parser')
+
+# print(BeautifulSoup(
+#     "<html><head></head><body>Sacr&eacute; bleu!</body></html>", "html.parser"))
+# BS4 TEST END
+
+# Beautiful Soup Test
+# soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
+# print(soup.prettify())
