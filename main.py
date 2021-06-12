c509f73122a26f328b56ccf260bcb63c4e6de20d
Sean Melody <seandmelody@gmail.com>
2021-06-12 11:15:50 -0700

    function added to be able to keep getting prices of coins

--- main.py
+++ main.py
@@ -31,18 +31,42 @@ def get_crypto_price(user_selected):
     return price
 
 
-# Using inquirer, give the user a list of coins to find the price of.
-# This is done to avoid user misspelling, or typing in something that is not a coin.
-crypto_options = [inquirer.List('crypto',
-                                message="What coin price would you like to check?: ",
-                                choices=['Bitcoin', 'Ethereum', 'Tether', 'Cardano', 'Dogecoin',
-                                         'XRP', 'Polkadot', 'Uniswap', 'Litecoin', 'Solana', 'Filecoin']
-                                )
-                  ]
-# Ask the question and set the response to a variable
-user_selected = inquirer.prompt(crypto_options)
-# Call the get_crypto_price function and send it the user selected coin.
-print_price = get_crypto_price(user_selected)
-# Print the results of the get_crypto_price function!
-# Yeay!
-print(print_price)
+def another():
+
+    pick_again = {inquirer.Confirm('again',
+                                   message="Do you want to get the price of another coin?: ",
+                                   default=True)
+                  }
+
+    run_again = inquirer.prompt(pick_again)
+
+    # print(run_again)
+
+    if run_again['again'] == True:
+        pick_a_coin()
+    else:
+        print("HODL! Goodbye!")
+
+
+def pick_a_coin():
+    # while True:
+
+    # Using inquirer, give the user a list of coins to find the price of.
+    # This is done to avoid user misspelling, or typing in something that is not a coin.
+    crypto_options = [inquirer.List('crypto',
+                                    message="What coin price would you like to check?: ",
+                                    choices=['Bitcoin', 'Ethereum', 'Tether', 'Cardano', 'Dogecoin',
+                                             'XRP', 'Polkadot', 'Uniswap', 'Litecoin', 'Solana', 'Filecoin']
+                                    )
+                      ]
+    # Ask the question and set the response to a variable
+    user_selected = inquirer.prompt(crypto_options)
+    # Call the get_crypto_price function and send it the user selected coin.
+    print_price = get_crypto_price(user_selected)
+    # Print the results of the get_crypto_price function!
+    # Yeay!
+    print(print_price)
+    another()
+
+
+pick_a_coin()
