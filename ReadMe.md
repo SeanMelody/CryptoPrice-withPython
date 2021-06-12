cb979758dacb10e66faaf50d5ee922b1726bd221
Sean Melody <seandmelody@gmail.com>
2021-06-12 11:27:11 -0700

    ReadMe added

---
+++ ReadMe.md
@@ -0,0 +1,53 @@
+# CryptoPrice with Python 
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+    Created using  Python, the user can get the price to 11 top Crypto coins.
+
+
+## Table of Contents
+
+  [Description](##Description)
+
+  [Installation](##Installation)
+
+  [Usage](##Usage)
+
+  [License](##License)
+
+  [Contributing](##Contributing)
+
+  [Tests](##Tests)
+
+
+## Installation
+
+    Must install bs4, inquirer and requests before running.
+
+## Usage
+
+    Use this project to quickly get prices to your favorite crypto coins in real time.
+
+## License
+
+    This application is covered under the MIT License
+
+## Contributing
+
+    Simply fork and submit a git pull to contribute
+
+## Tests
+
+    Make sure requirements are installed
+
+
+## Questions?
+
+  Check out my GitHub:
+
+  [github.com/SeanMelody](https://github.com/SeanMelody)
+
+  Or if you have any questions email me: 
+    seandmelody@gmail.com
--- main.py
+++ main.py
@@ -14,42 +14,48 @@ def get_crypto_price(user_selected):
     # Set up the print so that the user knows that it is working, and will have more inforation on what is diplayed.
     print('One ' + coin + ' costs:')
 
-# Get the Url to seach using Beautiful Soup and the selected coin
+    # Get the Url to seach using Beautiful Soup and the selected coin
     url = 'https://coinmarketcap.com/currencies/' + coin
 
-# Make the request
+    # Make the request
     HTML = requests.get(url)
 
-# Parse the data with Beautiful soup, using the html parser
+    # Parse the data with Beautiful soup, using the html parser
     soup = BeautifulSoup(HTML.text, 'html.parser')
 
-
-# Get the price from the webpage, that is located in the <div class="priceValue___11gHJ">
+    # Get the price from the webpage, that is located in the <div class="priceValue___11gHJ">
     price = soup.find('div', attrs={'class': 'priceValue___11gHJ'}).text
 
- # Return the price
+    # Return the price to be able to print it.
     return price
+
+# Function to ask user if they want to get another coin price using Inquirer Confirm
 
 
 def another():
-
+    # Inquirer confrim letting the user select 'y' or 'n'
     pick_again = {inquirer.Confirm('again',
                                    message="Do you want to get the price of another coin?: ",
                                    default=True)
                   }
 
+    # Set the response to a variable
     run_again = inquirer.prompt(pick_again)
 
     # print(run_again)
 
+    # If statement to see if the user selected yes or no.
+    # If yes, run the pick_a_coin function again
     if run_again['again'] == True:
         pick_a_coin()
+    # Else make a silly crypto joke and say goodbye.
     else:
         print("HODL! Goodbye!")
 
+# Main function to ask the user what coin they would like the price of
+
 
 def pick_a_coin():
-    # while True:
 
     # Using inquirer, give the user a list of coins to find the price of.
     # This is done to avoid user misspelling, or typing in something that is not a coin.
@@ -64,9 +70,10 @@ def pick_a_coin():
     # Call the get_crypto_price function and send it the user selected coin.
     print_price = get_crypto_price(user_selected)
     # Print the results of the get_crypto_price function!
-    # Yeay!
     print(print_price)
+    # Call the another function to see if the user would like to ask another question
     another()
 
 
+# Start the program by calling the pick_a_coin function!
 pick_a_coin()
