eada2f179bee925f16c8059dd8b353604b394321
Sean Melody <seandmelody@gmail.com>
2021-06-12 10:14:51 -0700

    Inquirer installed and working for coin selection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import inquirer
 
 # Printing Test Start
 # import string
@@ -26,26 +27,30 @@ import time
 # print(soup.prettify())
 
 
-def get_crypto_price(coin):
+def get_crypto_price(user_selected):
 
-    # Get the URLS's
-    # etherium = 'http://google.com/search?q=etherium+price'
-    # url = 'http://google.com/search?q=bitcoin+' + coin + 'price'
+    print(user_selected)
+    coin = user_selected['crypto']
+    print(coin)
+
+# Get the URLS's
+# etherium = 'http://google.com/search?q=etherium+price'
+# url = 'http://google.com/search?q=bitcoin+' + coin + 'price'
     url = 'https://coinmarketcap.com/currencies/' + coin
 
-    # Make  a request
+# Make  a request
     HTML = requests.get(url)
 
-    #   Parse!
+#   Parse!
     soup = BeautifulSoup(HTML.text, 'html.parser')
 
-    # Print
-    # print(soup.prettify())
+# Print
+# print(soup.prettify())
 
-    # Find the current price
+# Find the current price
     text = soup.find('div', attrs={'class': 'priceValue___11gHJ'}).text
 
-    # print(text)
+# print(text)
 
     return text
 
@@ -54,14 +59,20 @@ def get_crypto_price(coin):
 # price = get_crypto_price('bitcoin')
 # print(price + ' US Dollars')
 
-users_coin = input("Which coin price would you like?: ")
-print_price = get_crypto_price(users_coin)
-print(print_price)
+# users_coin = input("Which coin price would you like?: ")
+# print_price = get_crypto_price(users_coin)
+# print(print_price)
 # Create Function to show the price when it changes
 
-
-def loop():
-    last_price = -1
-    # Loop to continulously show the price
-    # while True:
-    # pick a coin:
+crypto_options = [inquirer.List('crypto',
+                                message="What coin price would you like to check?: ",
+                                choices=['Bitcoin', 'Ethereum', 'Tether', 'Binance',
+                                         'Cardano', 'Dogecoin', 'XRP', 'USD', 'Polkadot', 'Uniswap']
+                                )
+                  ]
+user_selected = inquirer.prompt(crypto_options)
+# print(user_selected)
+print_price = get_crypto_price(user_selected)
+print(print_price)
+# print_price = get_crypto_price(coin)
+# print(print_price)
